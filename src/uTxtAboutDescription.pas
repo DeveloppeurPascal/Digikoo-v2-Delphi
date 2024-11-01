95480bdec7db0bfff98a1d931c971a25fe34a3a4
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-11-01 17:15:29 +0100

    added the default project files after configuring the starter kit

---
+++ src/uTxtAboutDescription.pas
@@ -0,0 +1,157 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Gamolf FMX Game Template
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// The "Gamolf FMX Game Template" is both a "technical" example of a video
+/// game developed in Delphi with everything you need inside, and a reusable
+/// project template you can customize for your own games.
+///
+/// The files provided are fully functional. Numerous comments are included in
+/// the sources to explain how they work and what you need to copy, override
+/// or customize to make video games without starting from scratch.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://gametemplate.developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Gamolf-FMX-Game-Template
+///
+/// ***************************************************************************
+/// File last update : 2024-08-09T20:35:58.000+02:00
+/// Signature : b224ad6ad4baa80432a9dcbd2ed65ee1dc267399
+/// ***************************************************************************
+/// </summary>
+
+unit uTxtAboutDescription;
+
+interface
+
+function GetTxtAboutDescription(const Language: string;
+  const Recursif: boolean = false): string;
+
+implementation
+
+// For the languages codes, please use 2 letters ISO codes
+// https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+
+uses
+  System.SysUtils,
+  uConsts;
+
+const
+  CTxtEN = '''
+Digikoo offers all fans of numbers and Sudoku the chance to play with their neurons. It's a great way to keep your mind sharp.
+
+The rule is simple: put the same number only once per row and column on grids ranging from 2x2 to 9x9 squares.
+
+*****************
+* Publisher info
+
+This application was developed by Patrick Prémartin in Delphi.
+
+It is published by OLF SOFTWARE, a company registered in Paris (France) under the reference 439521725.
+
+****************
+* Personal data
+
+This program is autonomous in its current version. It does not depend on the Internet and communicates nothing to the outside world.
+
+We have no knowledge of what you do with it.
+
+No information about you is transmitted to us or to any third party.
+
+We use no cookies, no tracking, no stats on your use of the application.
+
+***************
+* User support
+
+If you have any questions or require additional functionality, please leave us a message on the application''s website or on its code repository.
+
+To find out more, visit https://digikoo.gamolf.fr
+
+''';
+   CTxtFR = '''
+Digikoo propose à tous les amateurs de chiffres et de Sudoku de jouer avec leurs neurones. Un très bon exercice pour garder un esprit au top.
+
+La règle est simple : ne mettre qu'une seule fois le même chiffre par ligne et colonne sur des grilles s'enchainant de 2x2 à 9x9 cases.
+
+*****************
+* Info éditeur
+
+Cette application a été développée par Patrick Prémartin en Delphi.
+
+Elle est éditée par OLF SOFTWARE, société enregistrée à Paris (France) sous la référence 439521725.
+
+****************
+* Données personnelles
+
+Ce programme est autonome dans sa version actuelle. Il ne dépend pas d'Internet et ne communique rien au monde extérieur.
+
+Nous n'avons aucune connaissance de ce que vous faites avec lui.
+
+Aucune information vous concernant n'est transmise à nous ou à des tiers.
+
+Nous n'utilisons pas de cookies, pas de tracking, pas de statistiques sur votre utilisation de l'application.
+
+***************
+* Assistance aux utilisateurs
+
+Si vous avez des questions ou si vous avez besoin de fonctionnalités supplémentaires, veuillez nous laisser un message sur le site web de l'application ou sur son dépôt de code.
+
+Pour en savoir plus, visitez https://digikoo.gamolf.fr
+
+''';
+  // CTxtIT = '';
+  // CTxtDE = '';
+  // CTxtJP = '';
+  // CTxtPT = '';
+  // CTxtES = '';
+
+function GetTxtAboutDescription(const Language: string;
+  const Recursif: boolean): string;
+var
+  lng: string;
+begin
+  lng := Language.tolower;
+  if (lng = 'en') then
+    result := CTxtEN
+  else if (lng = 'fr') then // France
+    result := CTxtFR
+    // TODO : add your translations here
+    // else if (lng = 'it') then // Italy
+    // result := CTxtIT
+    // else if (lng = 'de') then // Germany
+    // result := CTxtDE
+    // else if (lng = 'jp') then // Japan
+    // result := CTxtJP
+    // else if (lng = 'pt') then // Portugal
+    // result := CTxtPT
+    // else if (lng = 'es') then // Spain
+    // result := CTxtES
+    // etc...
+  else if not Recursif then
+    result := GetTxtAboutDescription(CDefaultLanguage, true)
+  else
+    raise Exception.Create('Unknow description for language "' +
+      Language + '".');
+end;
+
+end.
