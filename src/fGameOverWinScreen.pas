08835af6e90fe1899ca778f425a50c1c0395ed47
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-11-03 20:18:26 +0100

    filled game over (win) screen

--- src/fGameOverWinScreen.pas
+++ src/fGameOverWinScreen.pas
@@ -34,6 +34,9 @@ unit fGameOverWinScreen;
 
 interface
 
+// TODO : changer couleur d'affichage du text des crédits
+// TODO : ajouter saisie du pseudo pour enregistrement du score
+
 uses
   System.SysUtils,
   System.Types,
@@ -46,15 +49,30 @@ uses
   FMX.Forms,
   FMX.Dialogs,
   FMX.StdCtrls,
-  _ScenesAncestor;
+  _ScenesAncestor,
+  _ButtonsAncestor,
+  _SporglooButtonAncestor,
+  FMX.Layouts,
+  FMX.Objects,
+  FMX.ImgList,
+  udmv1_pictures;
 
 type
   TGameOverWinScreen = class(T__SceneAncestor)
+    VertScrollBox1: TVertScrollBox;
+    Glyph1: TGlyph;
+    Text1: TText;
+    Layout1: TLayout;
+    Layout2: TLayout;
+    btnEndGame: T__SporglooButtonAncestor;
+    procedure FrameResized(Sender: TObject);
+    procedure btnEndGameClick(Sender: TObject);
   private
   public
     procedure TranslateTexts(const Language: string); override;
     procedure ShowScene; override;
     procedure HideScene; override;
+    procedure BeforeFirstShowScene; override;
   end;
 
 implementation
@@ -64,26 +82,90 @@ implementation
 uses
   System.Messaging,
   uConsts,
-  uScene;
+  uScene,
+  uUIElements,
+  uDigikooGameData,
+  uSoundEffects;
 
 { TGameOverWinScreen }
+
+procedure TGameOverWinScreen.BeforeFirstShowScene;
+begin
+  inherited;
+  Text1.Font.Size := Text1.Font.Size * 2;
+end;
+
+procedure TGameOverWinScreen.btnEndGameClick(Sender: TObject);
+begin
+  TScene.Current := TSceneType.home; // TODO : à remplacer par HallOfFame;
+end;
+
+procedure TGameOverWinScreen.FrameResized(Sender: TObject);
+var
+  w: single;
+begin
+  if width > 500 then
+    w := 500
+  else
+    w := width - 20;
+
+  if Text1.width > w then
+  begin
+    Text1.margins.Left := (width - w) / 2;
+    Text1.margins.right := Text1.margins.Left;
+  end
+  else
+  begin
+    Text1.margins.Left := (width - Text1.width) / 2;
+    Text1.margins.right := Text1.margins.Left;
+  end;
+end;
 
 procedure TGameOverWinScreen.HideScene;
 begin
   inherited;
-  // TODO : à compléter
+  TUIItemsList.Current.RemoveLayout;
 end;
 
 procedure TGameOverWinScreen.ShowScene;
 begin
   inherited;
-  // TODO : à compléter
+  TUIItemsList.Current.NewLayout;
+  TUIItemsList.Current.AddControl(btnEndGame, nil, nil, nil, nil, true, true);
+
+  TSoundEffects.Current.Play(TSoundEffectType.Victoire);
+  // TODO : remplacer par quelque chose d'extraordinaire
+
+  FrameResized(self);
+
+  TDigikooGameData.DefaultGameData.StopGame;
 end;
 
 procedure TGameOverWinScreen.TranslateTexts(const Language: string);
+var
+  s: string;
 begin
   inherited;
-  // TODO : à compléter
+  if (Language = 'fr') then
+  begin
+    btnEndGame.Text := 'Menu'; // TODO : à remplacer par "Hall Of Fame"
+    s := 'Bravo !' + slinebreak + slinebreak;
+    s := s + 'Rares sont ceux qui sont arrivés au bout...' + slinebreak +
+      slinebreak;
+    if (TDigikooGameData.DefaultGameData.score > 0) then
+      s := s + 'Votre score : ' + TDigikooGameData.DefaultGameData.
+        score.ToString;
+  end
+  else
+  begin
+    btnEndGame.Text := 'Menu'; // TODO : à remplacer par "Hall Of Fame"
+    s := 'Bravo !' + slinebreak + slinebreak;
+    s := s + 'Few have ever reached the end...' + slinebreak + slinebreak;
+    if (TDigikooGameData.DefaultGameData.score > 0) then
+      s := s + 'Your score : ' + TDigikooGameData.DefaultGameData.
+        score.ToString;
+  end;
+  Text1.Text := s;
 end;
 
 initialization
@@ -98,7 +180,7 @@ TMessageManager.DefaultManager.SubscribeToMessage(TSceneFactory,
     begin
       NewScene := TGameOverWinScreen.Create(application.mainform);
       NewScene.Parent := application.mainform;
-      tscene.RegisterScene(TSceneType.GameOverWin, NewScene);
+      TScene.RegisterScene(TSceneType.GameOverWin, NewScene);
     end;
   end);
 
